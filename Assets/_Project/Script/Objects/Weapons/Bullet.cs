ef33eb44f422bccdc468356df2241f896fb84b93
RiccardoUnity <beninriccardo@gmail.com>
2025-06-10 21:05:03 +0200

    Bullet aggiornato
    - Alla nascita passargli il tag di chi spara, la forza e il danno;
    - Quando colpisce qualcosa passa il danno solo a chi di dovere o muore se tocca un muro;

--- Assets/_Project/Script/Objects/Weapons/Bullet.cs
+++ Assets/_Project/Script/Objects/Weapons/Bullet.cs
@@ -1,58 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private Transform _origin;             // chi spara 
-    public float speed = 10f;             // Velocità proiettile
-    public float lifeTime = 2f;           // Durata prima che si autodistrugga
-    public int damage = 1;                // Danno inflitto
-    public string Enemytag = "Enemy";     // Il tag che identifica il bersaglio
-    public string Playertag = "Player";   // Il tag che identifica il player
+    private string _originTag;             // chi spara 
+    [SerializeField] private float _speed = 10f;             // Velocità proiettile
+    [SerializeField] private float _lifeTime = 5f;           // Durata prima che si autodistrugga
+    private int _damage = 1;                // Danno inflitto
+    [SerializeField] private string _enemyTag = "Enemy";     // Il tag che identifica il bersaglio
+    [SerializeField] private string _playerTag = "Player";   // Il tag che identifica il player
 
-    private Rigidbody2D rigidbody2d;
+    private Rigidbody2D _rb2D;
     private void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        Destroy(gameObject, lifeTime); // Dopo lifeTime secondi si autodistrugge
+        _rb2D = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, _lifeTime); // Dopo lifeTime secondi si autodistrugge
     }
 
-    private void FixedUpdate()
+    public void ShootBullet (string origin, Vector2 force, int damage)
     {
-     
-        
-
+        _originTag = origin;
+        _damage = damage;
+        _rb2D.AddForce(force * _speed, ForceMode2D.Impulse);
     }
 
-
-    public void ShootBullet (Transform origin, Vector2 force)
-
-    {
-
-        _origin = origin;
-
-        rigidbody2d.AddForce(force * speed, ForceMode2D.Impulse);
-
-    }
     private void OnTriggerEnter2D(Collider2D other)
-    {     
-       
-        if (other.CompareTag(Enemytag))
-        {
-            
-            Destroy(gameObject); // Distrugge il proiettile
-        }
-        else if (other.CompareTag(Playertag))
-
-                {
-                    Destroy(gameObject); // Colpisce muro → distrugge proiettile
-                }
-        else
-            
     {
-     
-
+        LifeController lifeController = GetComponent<LifeController>();
+        if (lifeController != null)
+        {
+            //Enemy spara al Player o Player spara ad Enemy
+            if ((other.CompareTag(_enemyTag) && _originTag == _playerTag) || (other.CompareTag(_playerTag) && _originTag == _enemyTag))
+            {
+                lifeController.TakeDamage(_damage);
+            }
         }
+
+        Destroy(gameObject); // Distrugge il proiettile
     }
 }
