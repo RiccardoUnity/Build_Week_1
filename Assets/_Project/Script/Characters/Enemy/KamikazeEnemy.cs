70813a62d767917374163342d322f96119c0dddd
EdoCirone <edoardocirone@gmail.com>
2025-06-10 15:26:48 +0200

    Spawn nemici
    Ho fatto lo SpawnDeiNemici

--- Assets/_Project/Script/Characters/Enemy/KamikazeEnemy.cs
+++ Assets/_Project/Script/Characters/Enemy/KamikazeEnemy.cs
@@ -8,25 +8,25 @@ public class KamikazeEnemy : Enemy
 
     public override void EnemyMovement()
     {
-        if (_player != null)
-        {
+        //if (_player != null)
+        //{
             
 
-            Vector2 direction = (_player.transform.position - transform.position).normalized;
-            // Muovi l'enemy nella direzione del giocatore
-            transform.position = Vector2.MoveTowards(transform.position, _player.transform.position, _movement._speed * Time.deltaTime);
-            transform.rotation = Quaternion.LookRotation(Vector3.forward, direction); // Ruota l'enemy verso il giocatore
+        //    Vector2 direction = (_player.transform.position - transform.position).normalized;
+        //    // Muovi l'enemy nella direzione del giocatore
+        //    transform.position = Vector2.MoveTowards(transform.position, _player.transform.position, _movement._speed * Time.deltaTime);
+        //    transform.rotation = Quaternion.LookRotation(Vector3.forward, direction); // Ruota l'enemy verso il giocatore
 
-        }
+        //}
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject == _player.gameObject)
-        {
-            collision.gameObject.GetComponent<LifeController>().RemoveHp(10); // Rimuove 10 punti vita al giocatore quando l'enemy lo colpisce
-            _lifeController.RemoveHp(_maxHp); // Rimuove 100 punti vita all'enemy quando colpisce il giocatore
-        }
+        //if (collision.gameObject == _player.gameObject)
+        //{
+        //    collision.gameObject.GetComponent<LifeController>().RemoveHp(10); // Rimuove 10 punti vita al giocatore quando l'enemy lo colpisce
+        //    _lifeController.RemoveHp(_maxHp); // Rimuove tutti i punti vita all'enemy quando colpisce il giocatore
+        //}
     }
 
 }
--- Assets/_Project/Script/Manager/EnemyInstantiate.cs
+++ Assets/_Project/Script/Manager/EnemyInstantiate.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class EnemyInstantiate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float minDis = 3f;
+    [SerializeField] float minPlayerDis = 8f;
+
+    [SerializeField] int enemyNumbers = 10;
+    [SerializeField] int enemySpawnRange = 10;
+
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] List<GameObject> enemyList = new List<GameObject>();
+
     void Start()
     {
-        
+
+        SpawnEnemies();// Spawno gli enemy all'inizio del gioco
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        enemyList.RemoveAll(enemy => enemy == null); // Rimuove gli enemy null dalla lista
+
+        if (enemyList.Count <= 0) // Se non ci sono più enemy nella lista, spawn nuovi enemy
+        {
+            SpawnEnemies();
+        }
+    }
+
+    private void SpawnEnemies()
+
+    {
+        int maxTry = 0; // Contatore per il numero di tentativi di spawn
+
+        int i = 0;// Contatore per il numero di enemy spawnati
+
+        while (i < enemyNumbers && maxTry < 100)
+        {
+            // Genera una posizione casuale per lo spawn degli enemy
+            Vector2 spawnPosition = new Vector2(
+                Random.Range(-enemySpawnRange, enemySpawnRange),
+                Random.Range(-enemySpawnRange, enemySpawnRange)
+            );
+            // Controlla se la posizione è accettabile per lo spawn
+            if (AcceptablePosition(spawnPosition))
+            {
+                GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+                enemyList.Add(enemy);
+                i++;
+            }
+            // Incrementa il contatore dei tentativi
+            maxTry++;
+
+            if (maxTry == 100)
+            {
+                // Se sono stati fatti 100 tentativi senza successo, esce dal ciclo
+                Debug.Log("C'abbiamo provato! ");
+            }
+        }
+    }
+
+
+
+    bool AcceptablePosition(Vector2 spawnPosition) // Controlla se la posizione di spawn è accettabile
+    {
+        foreach (GameObject precedentEnemy in enemyList)
+        {
+            float distance = Vector2.Distance(spawnPosition, precedentEnemy.transform.position);// Calcola la distanza tra la posizione di spawn e la posizione degli enemy già presenti
+            float playerDistance = Vector2.Distance(spawnPosition, FindAnyObjectByType<PlayerController>().transform.position); // Calcola la distanza tra la posizione di spawn e la posizione del giocatore
+
+            if (distance < minDis || playerDistance < minPlayerDis)//Confronta le distanze
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
