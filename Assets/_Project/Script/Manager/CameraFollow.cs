a50353f9b454ce7dbc975185493445f77a81b717
Fed511 <federicouni350@gmail.com>
2025-06-12 17:05:23 +0200

    Updated camera follow script

--- Assets/_Project/Script/Manager/CameraFollow.cs
+++ Assets/_Project/Script/Manager/CameraFollow.cs
@@ -2,27 +2,48 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] Transform _target; // Il target da seguire
 
+    public Transform target;
+    private Vector3 offset;
 
-
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-
-        if (_target != null) // Controlla se il target è stato assegnato
+        if (target == null)
         {
-            Vector3 newPosition = _target.position; // Prende la posizione del target
-
-            newPosition.z = transform.position.z; // Mantiene la stessa profondità della camera
-
-            transform.position = newPosition; // Aggiorna la posizione della camera
+            Debug.LogError("CameraFollow: Target non assegnato.");
+            return;
         }
 
-        else
-        {
-            transform.position = new Vector3(0f, 0f, transform.position.z);  // Se il target non è assegnato, imposta la posizione della camera al centro della scena
-        }
-
+        offset = transform.position - target.position;
     }
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 newPosition = target.position + offset;
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+    }
+
+    //Edo
+    //[SerializeField] Transform _target; // Il target da seguire
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    if (_target != null) // Controlla se il target è stato assegnato
+    //    {
+    //        Vector3 newPosition = _target.position; // Prende la posizione del target
+
+    //        newPosition.z = transform.position.z; // Mantiene la stessa profondità della camera
+
+    //        transform.position = newPosition; // Aggiorna la posizione della camera
+    //    }
+
+    //    else
+    //    {
+    //        transform.position = new Vector3(0f, 0f, transform.position.z);  // Se il target non è assegnato, imposta la posizione della camera al centro della scena
+    //    }
+
+    //}
 }
